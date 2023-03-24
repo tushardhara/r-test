7a9cf4b9fbe4a478bcf3e10f3f6643c6e9e51147
tushardhara <tushardharaster@gmail.com>
2023-03-24 21:34:20 +0530

    asda

---
+++ index.js
@@ -0,0 +1,14 @@
+const Redis = require('ioredis');
+const client = new Redis({
+    host: 'redis-master.rehook-redis.svc',
+    port: 6379,
+    password: 'dSO7HOi7aY'
+});
+
+client.ping((error, result) => {
+  if (error) {
+    console.log("Error:", error);
+  } else {
+    console.log("Redis connection is working:", result);
+  }
+});
